f354766941a3ca6688e4ee83a63934c6d50c9725
MauricioGedrat <mauriciogedrat@gmail.com>
2023-04-20 08:12:55 -0300

    Sem video call

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,4 +1,3 @@
-import VideoChat from "@/components/CallVideo";
 import { Header } from "@/components/Header";
 import React from "react";
 
@@ -6,7 +5,6 @@ export default function Register() {
   return (
     <>
       <Header />
-      <VideoChat />
     </>
   );
 }
